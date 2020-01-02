94f760cd3d88c5a0ef85e133b0417ede0da4bc51
Alexander Bruy <alexander.bruy@gmail.com>
2020-01-02 12:43:42 +0200

    tests placeholder

---
+++ tests/test_exiftool.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+    test_exiftool.py
+    ---------------------
+    Date                 : January 2020
+    Copyright            : (C) 2020 by Alexander Bruy
+    Email                : alexander dot bruy at gmail dot com
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+__author__ = 'Alexander Bruy'
+__date__ = 'January 2020'
+__copyright__ = '(C) 2020, Alexander Bruy'
+
+import unittest
+
+
+class TextExifTool(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
